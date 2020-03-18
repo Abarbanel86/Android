4659a285123023ef1160687544fa09585f0aa134
Abarbanel86 <abarbanel86@gmail.com>
2020-03-18 14:56:19 +0200

    18032020

---
+++ FlickerAPp/app/src/main/java/com/tomabarbanel/flickerapp/GetRawData.kt
@@ -0,0 +1,63 @@
+package com.tomabarbanel.flickerapp
+
+import android.os.AsyncTask
+import android.util.Log
+import java.io.IOException
+import java.lang.Exception
+import java.net.MalformedURLException
+import java.net.URL
+
+enum class DownloadStatus {
+    OK, IDLE, NOT_INITIALIZED, FAILED_OR_EMPTY, PERMISSIONS_ERROR, ERROR
+}
+
+private const val TAG = "GetRawData"
+
+class GetRawData(private val listener: MainActivity) : AsyncTask<String, Void, String>() {
+    private var status = DownloadStatus.IDLE
+
+//    fun setDownloadCompleteListener(callBackObject: MainActivity) {
+//        listener = callBackObject
+//    }
+
+    override fun doInBackground(vararg params: String?): String {
+        if (params[0] == null) {
+            status = DownloadStatus.NOT_INITIALIZED
+
+            return "URL string empty"
+        }
+
+        try {
+            status = DownloadStatus.OK
+
+            return URL(params[0]).readText()
+        } catch (e: Exception) {
+            val errorMessage = when (e) {
+                is MalformedURLException -> {
+                    status = DownloadStatus.NOT_INITIALIZED
+                    "doInBackGround: invalid URL ${e.message}"
+                }
+                is IOException -> {
+                    status = DownloadStatus.FAILED_OR_EMPTY
+                    "doInBackGround: IO Exception reading data ${e.message}"
+                }
+                is SecurityException -> {
+                    status = DownloadStatus.PERMISSIONS_ERROR
+                    "doInBackGround: Security exception ${e.message}"
+                }
+                else -> {
+                    status = DownloadStatus.ERROR
+                    "doInBackground unknown error ${e.message}"
+                }
+            }
+            Log.e(TAG, errorMessage)
+
+            return errorMessage
+        }
+    }
+
+    override fun onPostExecute(result: String) {
+        Log.d(TAG, "omPostExecute called ${result}")
+        listener.onDownloadComplete(result, status)
+    }
+}
---
+++ FlickerAPp/app/src/main/java/com/tomabarbanel/flickerapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.tomabarbanel.flickerapp
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val TAG = "MainActivity"
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
+        val getRawData = GetRawData(this)
+        //getRawData.setDownloadCompleteListener(this)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        getRawData.execute(getString(R.string.DownloadURL))
+
+        Log.d(TAG, "onCreate ends")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, "onCreateOptionsMenu called")
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        Log.d(TAG, "onOptionItemSelected called")
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+//    companion object {
+//        private val TAG = "MainActivity"
+//    }
+
+    fun onDownloadComplete(data: String, status: DownloadStatus) {
+        if(status == DownloadStatus.OK) {
+            Log.d(TAG, "onDownloadComplete, download completed data is ${data}")
+        } else {
+            Log.d(TAG, "onDownloadComplete download failed with error ${data}")
+        }
+    }
+}
